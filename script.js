9be83c719cc0455dd8cb71cf90050d440db5412b
Zaynokov <vlerii99@mail.ru>
2022-08-07 19:01:17 +0300

    Lesson 2

--- script.js
+++ script.js
@@ -7,22 +7,54 @@ const goods = [
     {title: 'Squier', price: 25000, image: default_image},
 ];
 
-const renderGoodsItem = ({title='Гитара', price='Дорого', image=default_image}) => {
+
+class GoodsItem {
+  constructor({title='Гитара', price=0, image=default_image}) {
+    this.title = title;
+    this.price = price;
+    this.image = image;
+  }
+
+  render() {
     return `
     <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="${image}" alt="Card image cap">
+    <img class="card-img-top" src="${this.image}" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">${title}</h5>
-      <p class="card-text">Цена: ${price}</p>
+      <h5 class="card-title">${this.title}</h5>
+      <p class="card-text">Цена: ${this.price}</p>
       <a href="#" class="btn btn-primary">В корзину</a>
     </div>
   </div>
     `;
-};
-
-const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+  }
 }
 
-renderGoodsList(goods);
+class GoodsList {
+  goods = [];
+
+  fetchGoods() {
+    this.goods = goods;
+  }
+
+  totalPrice() {
+    let result = 0;
+    this.goods.forEach(item => {
+      result += item.price
+    })
+    document.querySelector('.total-price').innerHTML = 'Общая стоимость - ' + result
+  }
+
+  render() {
+    let goodsList = this.goods.map((item) => {
+      const goodsItem = new GoodsItem(item);
+      return goodsItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+  }
+}
+
+const goodsList = new GoodsList();
+
+goodsList.fetchGoods()
+goodsList.render()
+goodsList.totalPrice()
